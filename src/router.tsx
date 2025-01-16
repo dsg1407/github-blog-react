45f83d3a0955601ab7b7181f1112c62713ddc267
Daniel S Goncalves <dsg1407@gmail.com>
2025-01-16 16:44:02 -0300

    feat: first commit

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/home'
+import { Issue } from './pages/issue'
+import { DefaultLayout } from './layouts/default-layout'
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/issue/*" element={<Issue />} />
+      </Route>
+    </Routes>
+  )
+}
