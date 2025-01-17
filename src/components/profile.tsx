57a55663a8d0d613e10133e635dbdca4c49049f3
Daniel S Goncalves <dsg1407@gmail.com>
2025-01-16 23:58:28 -0300

    feat: add profile components props and type

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -6,11 +6,23 @@ import {
 } from 'react-icons/fa6'
 import { Link } from './link'
 
-export function Profile() {
+type ProfileProps = {
+  user: {
+    name: string
+    login: string
+    avatar_url: string
+    html_url: string
+    bio: string
+    company: string
+    followers: number
+  }
+}
+
+export function Profile({ user }: ProfileProps) {
   return (
     <section className="w-full p-8 rounded-[10px] bg-base-profile flex items-center gap-8">
       <img
-        src="https://github.com/rocketseat-education.png"
+        src={user.avatar_url}
         alt=""
         className="w-[148px] h-[148px] object-cover rounded-lg ml-2"
       />
@@ -19,30 +31,28 @@ export function Profile() {
         <div>
           <div className="flex items-center justify-between">
             <h1 className="text-2xl font-bold leading-[130%] text-base-title my-2">
-              Rocketseat Education
+              {user.name}
             </h1>
-            <Link href="/" target="_blank">
+            <Link href={user.html_url} target="_blank">
               Github <FaUpRightFromSquare className="leading-[0]" />
             </Link>
           </div>
-          <p className="">
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-            volutpat pulvinar vel mass.
-          </p>
+          <p className="">{user.bio}</p>
         </div>
         <div className="flex gap-6">
           <span className="flex items-center gap-2 text-base-subtitle">
             <FaGithub className="text-base-label" />
-            cameronwll
+            {user.login}
           </span>
-          <span className="flex items-center gap-2 text-base-subtitle">
-            <FaBuilding className="text-base-label" />
-            Rocketseat
-          </span>
+          {user.company && (
+            <span className="flex items-center gap-2 text-base-subtitle">
+              <FaBuilding className="text-base-label" />
+              {user.company}
+            </span>
+          )}
           <span className="flex items-center gap-2 text-base-subtitle">
             <FaUserGroup className="text-base-label" />
-            32 seguidores
+            {user.followers} seguidores
           </span>
         </div>
       </div>
