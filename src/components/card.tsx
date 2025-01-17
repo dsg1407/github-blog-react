9a1e1cd2ca27bc26a2ff0d4d7735356c85e0a356
Daniel S Goncalves <dsg1407@gmail.com>
2025-01-17 00:03:22 -0300

    fix: adjust card component height

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ interface CardProps extends React.HTMLProps<HTMLAnchorElement> {
 export function Card({ title, date, description, ...rest }: CardProps) {
   return (
     <a
-      className="p-8 flex flex-col gap-5 bg-base-post border-2 border-base-post rounded-[10px] hover:transition-all hover:duration-300 hover:border-base-label cursor-pointer"
+      className="p-8 h-64 flex flex-col gap-5 bg-base-post border-2 border-base-post rounded-[10px] hover:transition-all hover:duration-300 hover:border-base-label cursor-pointer"
       {...rest}
     >
       <div className="flex items-center justify-between gap-4">
